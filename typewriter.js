f8c71873f91c1857456adf0f8fa440441b1d0d2d
Annnie Rahmati <beaatris@gmail.com>
2020-06-22 18:18:00 +0000

    Added typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+let sentence = "hello there from lighthouse labs";
+sentence =  sentence + '\n';
+
+let i = 0;
+for (const char of sentence){
+  setTimeout(() => {
+    process.stdout.write(char);} , (1000 + i));
+i += 100;
+};
+
